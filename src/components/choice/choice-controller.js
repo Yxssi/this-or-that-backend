4f644a1fa631a332413270eaf62edcef2c4565fd
Yessi yessi-y-munguengui <yessi.yessimunguengui@gmail.com>
2022-12-25 21:44:41 +0100

    fix: add cruds controller

--- src/components/choice/choice-controller.js
+++ src/components/choice/choice-controller.js
@@ -1,10 +1,52 @@
 import Choice from "#components/choice/choice-model.js";
 
+const choiceValidationSchema = Joi.object({
+  first_choice_image_url: Joi.string().required(),
+  second_choice_image_url: Joi.string().required(),
+  first_choice_title: Joi.string().required(),
+  second_choice_title: Joi.string().required(),
+});
+
 export async function index(ctx) {
   try {
     const choices = await Choice.find({});
     ctx.ok(choices);
   } catch (e) {
+    ctx.badRequest({ message: e.message });
+  }
+}
+
+export async function create(ctx) {
+  try {
+    const { error, value } = choiceValidationSchema.validate(ctx.request.body);
+    if (error) throw new Error(error);
+    const choice = await Choice.create(value);
+    ctx.ok(choice);
+  } catch (e) {
+    ctx.badRequest({ message: e.message });
+  }
+}
+
+export async function update(ctx) {
+  try {
+    const { error, value } = choiceValidationSchema.validate(ctx.request.body);
+    if (error) throw new Error(error);
+
+    const choice = await Choice.findByIdAndUpdate(ctx.params.id, value, {
+      runValidators: true,
+      new: true,
+    });
+    ctx.ok(choice);
+  } catch (e) {
+    ctx.badRequest({ message: e.message });
+  }
+}
+
+export async function del(ctx) {
+  try {
+    await Choice.findByIdAndDelete(ctx.params.id);
+    ctx.ok();
+  } catch (error) {
     ctx.badRequest({ message: e.message });
   }
 }
--- src/components/choice/choice-routes.js
+++ src/components/choice/choice-routes.js
@@ -4,9 +4,9 @@ import * as ChoiceControllers from "#components/choice/choice-controller.js";
 const choices = new Router();
 
 choices.get("/", ChoiceControllers.index);
-// choices.get("/:id", ChoicesControllers.id)
-// choices.post("/", ChoicesControllers.create);
-// choices.put("/:id", ChoicesControllers.update);
-// choices.del("/:id", ChoicesControllers.del);
+choices.get("/:id", ChoicesControllers.id);
+choices.post("/", ChoicesControllers.create);
+choices.put("/:id", ChoicesControllers.update);
+choices.del("/:id", ChoicesControllers.del);
 
 export default choices;
